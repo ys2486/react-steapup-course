4623fcd7ca76ead267dc5b8839ed0736908dbf9f
ys2486 <yusaku19940728@yahoo.co.jp>
2022-03-13 08:11:42 +0900

    basic files save(when you start use these files)

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import './App.css';
-import { ChildArea } from './ChildArea';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
-  const [open, setOpen] = useState(false);
-
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
-
-  const onChangeText = (event) => {
-    setText(event.target.value);
-    console.log(text);
-  };
-
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
-
-  const onClickClose = useCallback(() => {
-    console.log('onClickCloseが呼び出されましたーーーー');
-    setOpen(false);
-  }, []);
-
-  return (
-    <div>
-      <input type="text" onChange={onChangeText} value={text} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
-    </div>
-  );
+  return <div>a</div>;
 }
 
 export default App;
